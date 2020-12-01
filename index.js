1b90547d89781fa502268b6a3c4002c19074bfb1
PatOn <mahomed2278@gmail.com>
2020-12-01 13:09:30 +0000

    Working Proj

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNumber1 =(Math.floor(Math.random() * 6)+1);
+var strToNumber1 = ("images/dice" + randomNumber1 + ".png");
+document.querySelector(".img1").setAttribute("src", strToNumber1);
+
+
+var randomNumber2 = (Math.floor(Math.random()*6)+1);
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png")
+
+
+// console.log(randomNumber1 + "and" + randomNumber2);
+
+// alert(strToNumber1+ " and " + randomNumber2);
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").textContent="Player 1 Won";
+  // alert("Player 1 Won");
+} else if (randomNumber1 < randomNumber2){
+  document.querySelector("h1").textContent="Player 2 Won";
+  // alert("Player 2 Won");
+} else {
+  document.querySelector("h1").textContent="Draw";
+  // alert("Draw");
+}
