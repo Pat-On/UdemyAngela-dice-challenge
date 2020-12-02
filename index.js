b4d440fdf27fcc350530b5622804dbaecb9c86dc
PatOn <mahomed2278@gmail.com>
2020-12-02 16:47:46 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+
 var randomNumber1 =(Math.floor(Math.random() * 6)+1);
+
 var strToNumber1 = ("images/dice" + randomNumber1 + ".png");
 document.querySelector(".img1").setAttribute("src", strToNumber1);
-
 
 var randomNumber2 = (Math.floor(Math.random()*6)+1);
 document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png")
